53dea8df16b70cd778b67ecdea83af4328e5872d
Habib Mohsin <HabibMohsin034@gmail.com>
2024-03-18 13:12:46 +0500

    8th's Chapter Question Answers are complete

--- task_01_to_20.js
+++ task_01_to_20.js
@@ -116,3 +116,140 @@
 
 // don't use JavaScript's keywords in Varialbe Namme
 // var var = "Habib Moosa";
+
+// Chapter 5 (Math Expression I)
+
+// Ans No.1
+// var arithmeticOperator = 35 / 7;
+// console.log(arithmeticOperator);
+
+// Ans No.2
+// var num = 20 % 6;
+// console.log(num);
+// Result is 2
+
+// Ans No.3
+// var largeNum = 1000 * 2000;
+// console.log(largeNum);
+
+// Ans No.4
+// var num1 = 150;
+// var num2 = num1 - 25;
+// console.log(num2);
+
+// Ans No.5
+// var num1 = 6;
+// var num2 = 18;
+// var remainder = num1 % num2;
+// console.log(remainder, "Result");
+
+// Ans No.6
+// alert(2*6);
+
+// Chapter 6 (Math Expression II)
+
+// Ans No.1
+// var x = 5;
+// x++;
+// console.log(x);
+
+// Ans No.2
+// var x = 100;
+// x--;
+// console.log(x);
+
+// Ans No.3
+// var x = 50;
+// var y = x++;
+// console.log(y);
+// Result is 50
+
+// Ans No.4
+// var y = 50;
+// var z = --y;
+// console.log(y);
+// Result is 49
+
+// Ans No.5
+// var num = 100;
+// var newNum = num++;
+// console.log(newNum);
+
+// Ans No.6
+// var originalValue = 100;
+// var othervalue = ++originalValue;
+// console.log(othervalue)
+
+// Ans No.7
+// var num = 10;
+// alert(++num);
+
+// Chapter 7 (Math Expression III)
+
+// Ans No.1
+// var calculatedNum = 2 + (2*6);
+// console.log(calculatedNum)
+// Result is 14
+
+// Ans No.2
+// var calculatedNum = (2+2)*6;
+// console.log(calculatedNum);
+// Result is 24
+
+// Ans No.3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+// Result is 24
+
+// Ans No.4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum);
+// Result is 18
+
+// Ans No.5
+// var calculatedNum = (2 + 2) * 4 + 10;
+// console.log(calculatedNum);
+// Result is 26 in result plus 10 than achieve 56
+
+// Ans No.6
+// var calculatedNum = 2 + (2 * 4) + 10;
+// console.log(calculatedNum);
+// Result is 20
+
+// Ans No.7
+// var calculatedNum = 4 / (2 * 4);
+// console.log(calculatedNum);
+// Result is 0.5
+
+// Chapter 8 (Concatenating Text Strings)
+
+// Ans No.1
+// var num = "2" + "3";
+// console.log(num);
+// Result is 23
+
+// Ans No.2
+// message = ("Hello," + "Dolly");
+// console.log(message);
+// Result is Hello,Dolly
+
+// Ans No.3
+// alert ("33" + 3);
+// Result is 333
+
+// Ans No.4
+// alert ("Pakistan " + "Zindabad");
+// Result is Pakistan Zindabad
+
+// Ans No.5
+// var alphaNum = "HabibMohsin" + 123;
+// console.log(alphaNum)
+// Result is HabibMohsin123
+
+// Ans No.6
+// var firstName = "Habib"
+// var lastName = "Mohsin"
+// var fullName = firstName + lastName
+// console.log(fullName)
+// Result is HabibMohsin
+

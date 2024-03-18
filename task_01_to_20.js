002ef77b051bdca71e4b00ea77239ce5b54d6106
Habib Mohsin <HabibMohsin034@gmail.com>
2024-03-18 07:30:32 +0500

    start working

---
+++ task_01_to_20.js
@@ -0,0 +1,45 @@
+// Chapter 1 (Alerts)
+
+// Ans No.1
+// alert("Habib");
+// alert("Mohsin");
+// alert("habibmohsin034@gmail.com");
+// alert("03412449832");
+// alert("abc123456");
+
+// Ans No.2
+// alert("You're learning JavaSeript");
+
+// Ans No.2
+// alert("Marhaba Habibi");
+
+// Chapter 2 (Variables for strinf)
+
+// Ans No.1
+// var fullName ="Habib Mohsin";
+// console.log(fullName);
+
+// Ans No.2
+// var fullName = "";
+// fullName = "Habib Moosa";
+// console.log(fullName);
+
+// Ans No.3
+// var teamName = "FAMAHA Group";
+// alert(teamName);
+
+// Ans No.4
+// var bestMan = "Charlie";
+// bestMan = "Habib Mohsin";
+// console.log(bestMan);
+
+// Chapter 3 (Variables for numbers)
+
+// Ans No.1
+// var caseQty = "caseQty";
+// console.log(caseQty);
+
+// Ans No.2
+// var caseQty = "";
+// caseQty = 144;
+// console.log(caseQty);
